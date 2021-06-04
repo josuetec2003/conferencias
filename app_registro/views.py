99ab37629a0eaffe94974905a925b507212ea322
Josue Alvarez <josuetec2003@gmail.com>
2021-06-03 20:49:14 -0600

    uso de message de django y se implemento algo jquery con una caja de busqueda

--- app_registro/views.py
+++ app_registro/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib import messages
 from .models import Participante
 
 def index(request):
@@ -16,21 +17,44 @@ def participantes(request):
         p = Participante(nombre=nombre, apellido=apellido, correo=correo, twitter=twitter)
         p.save()
 
-        ctx = {
-            'participantes': Participante.objects.all().order_by('nombre')
-        }
+        messages.add_message(request, messages.INFO, f'El participante {nombre} {apellido} ha sido registrado con éxito')
+
+        # return JsonResponse({
+        #     'nombre': nombre,
+        #     'apellido': apellido,
+        #     'correo': correo,
+        #     'twitter': twitter,
+        #     'OK': True,
+        #     'msj': 'El participante ha sido registrado con éxito'
+        # })
+
+        # ctx = {
+        #     'participantes': Participante.objects.all().order_by('nombre')
+        # }
 
         # return HttpResponse('El participante ha sido registrado')
-        return render(request, 'registro/participantes.html', ctx)
+        # return render(request, 'registro/participantes.html', ctx)
     
     # Metodo GET, PUT, PATCH, DELETE
 
     # La primera consulta: select * from participantes order by nombre desc
     # Realizar un Queryset con el ORM de Django
-    data = Participante.objects.all().order_by('nombre')
+    q = request.GET.get('q')
+
+    if q:
+        data = Participante.objects.filter(nombre__startswith=q).order_by('nombre')
+
+        '''
+            select * 
+            from participantes
+            where nombre like 'n%'
+        '''
+    else:
+        data = Participante.objects.all().order_by('nombre')
 
     ctx = {
-        'participantes': data
+        'participantes': data,
+        'q': q
     }
 
     return render(request, 'registro/participantes.html', ctx)
